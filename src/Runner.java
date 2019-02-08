9ad491ca1478b9d96ba9e1d754410d7ac11ab304
Mariah Baysic <mjpb123@gmail.com>
2019-02-09 02:18:00 +0800

    Runner.java - accessed SpeedConverter; SpeedConverter.java - converts km/h to mi/h

--- src/Runner.java
+++ src/Runner.java
@@ -3,7 +3,11 @@ public class Runner {
     public static void main(String[] args) {
         Inputs i = new Inputs();
 
-
+//        SpeedConverter.printConversion(1.5); //1
+//        SpeedConverter.printConversion(10.25); //6
+//        SpeedConverter.printConversion(-5.6); //invalid
+//        SpeedConverter.printConversion(25.42); //16
+//        SpeedConverter.printConversion(75.114); //47
     }
 
 }
