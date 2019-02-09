80ed868bfb0c440467fa2a3bf22a328933b2ab20
Mariah Baysic <mjpb123@gmail.com>
2019-02-09 23:08:08 +0800

    Runner.java - called BarkingDog; BarckingDog.java - return boolean to determine if you should wake up when the dog barks

--- src/Runner.java
+++ src/Runner.java
@@ -9,9 +9,14 @@ public class Runner {
 //        SpeedConverter.printConversion(25.42); //16
 //        SpeedConverter.printConversion(75.114); //47
 
-        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
-        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
-        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+//        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+//        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
+//        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+
+        System.out.println(BarkingDog.shouldWakeUp(true, 1));
+        System.out.println(BarkingDog.shouldWakeUp(false, 2));
+        System.out.println(BarkingDog.shouldWakeUp(true, 8));
+        System.out.println(BarkingDog.shouldWakeUp(true, -1));
     }
 
 }
