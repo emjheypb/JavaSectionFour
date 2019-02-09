ba90c2b252a2c37ee1e776c4185de125af451686
Mariah Baysic <mjpb123@gmail.com>
2019-02-09 23:51:12 +0800

    Runner.java - commented out DecimaComparator, added EqualSumChecker; EqualSumChecker.java - determines if the sum of a 1st and 2nd number is equal to the 3rd

--- src/Runner.java
+++ src/Runner.java
@@ -24,10 +24,14 @@ public class Runner {
 //        System.out.println(LeapYear.isLeapYear(2000));
 //        System.out.println(LeapYear.isLeapYear(1924));
 
-        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.1756, -3.175));
-        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.175, 3.176));
-        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.0, 3.0));
-        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.123, 3.123));
+//        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+//        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.175, 3.176));
+//        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.0, 3.0));
+//        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.123, 3.123));
+
+        System.out.println(EqualSumChecker.hasEqualSum(1,1,1));
+        System.out.println(EqualSumChecker.hasEqualSum(1,1,2));
+        System.out.println(EqualSumChecker.hasEqualSum(1,-1,0));
     }
 
 }
