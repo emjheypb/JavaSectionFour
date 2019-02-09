33c4c97ad354f2f287a90b7e4294e7c9b6de8cfc
Mariah Baysic <mjpb123@gmail.com>
2019-02-10 01:35:40 +0800

    SecondsAndMinutesChallenge.java - removed else statements and {} for if

--- src/SecondsAndMinutesChallenge.java
+++ src/SecondsAndMinutesChallenge.java
@@ -1,24 +1,24 @@
 public class SecondsAndMinutesChallenge {
 
     public static String getDurationString(int minutes, int seconds) {
-        if (minutes >= 0 && seconds >= 0 && seconds <= 59) {
+        if (minutes >= 0 && seconds >= 0 && seconds <= 59)
             return zeroPad(minutes / 60) + "h " + zeroPad(minutes % 60) + "m " + zeroPad(seconds) + "s";
-        } else
-            return "Invalid Value";
+
+        return "Invalid Value";
     }
 
     public static String getDuration(int seconds) {
-        if (seconds >= 0) {
+        if (seconds >= 0)
             return getDurationString(seconds / 60, seconds % 60);
-        } else
-            return "Invalid Value";
+
+        return "Invalid Value";
     }
 
     public static String zeroPad(int num) {
         if (num / 10 < 1)
             return "0" + num;
-        else
-            return Integer.toString(num);
+
+        return Integer.toString(num);
     }
 
 }
