d6b7c8c2541eced97f80283bb3a8556a09592557
Mariah Baysic <mjpb123@gmail.com>
2019-02-10 01:09:33 +0800

    Runner.java - commenter out everything, added SecondsAndMinutesChallenge; SecondsAndMinutesChallenge.java - returns XXh XXm XXs text from given minutes and seconds values

--- src/Runner.java
+++ src/Runner.java
@@ -3,51 +3,65 @@ public class Runner {
     public static void main(String[] args) {
         Inputs i = new Inputs();
 
-        SpeedConverter.printConversion(1.5);
-        SpeedConverter.printConversion(10.25);
-        SpeedConverter.printConversion(-5.6);
-        SpeedConverter.printConversion(25.42);
-        SpeedConverter.printConversion(75.114);
+//        SpeedConverter.printConversion(1.5);
+//        SpeedConverter.printConversion(10.25);
+//        SpeedConverter.printConversion(-5.6);
+//        SpeedConverter.printConversion(25.42);
+//        SpeedConverter.printConversion(75.114);
+//
+//        System.out.println();
+//
+//        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+//        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
+//        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+//
+//        System.out.println();
+//
+//        System.out.println(BarkingDog.shouldWakeUp(true, 1));
+//        System.out.println(BarkingDog.shouldWakeUp(false, 2));
+//        System.out.println(BarkingDog.shouldWakeUp(true, 8));
+//        System.out.println(BarkingDog.shouldWakeUp(true, -1));
+//
+//        System.out.println();
+//
+//        System.out.println(LeapYear.isLeapYear(-1600));
+//        System.out.println(LeapYear.isLeapYear(1600));
+//        System.out.println(LeapYear.isLeapYear(2017));
+//        System.out.println(LeapYear.isLeapYear(2000));
+//        System.out.println(LeapYear.isLeapYear(1924));
+//
+//        System.out.println();
+//
+//        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+//        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.175, 3.176));
+//        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.0, 3.0));
+//        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.123, 3.123));
+//
+//        System.out.println();
+//
+//        System.out.println(EqualSumChecker.hasEqualSum(1,1,1));
+//        System.out.println(EqualSumChecker.hasEqualSum(1,1,2));
+//        System.out.println(EqualSumChecker.hasEqualSum(1,-1,0));
+//
+//        System.out.println();
+//
+//        System.out.println(TeenNumberChecker.hasTeen(9,99,19));
+//        System.out.println(TeenNumberChecker.hasTeen(23,15,42));
+//        System.out.println(TeenNumberChecker.hasTeen(22,23,34));
+//
+//        System.out.println();
+//
+        System.out.println(SecondsAndMinutesChallenge.getDurationString(70, 59));
+        System.out.println(SecondsAndMinutesChallenge.getDurationString(0, 0));
+        System.out.println(SecondsAndMinutesChallenge.getDurationString(70, 61));
+        System.out.println(SecondsAndMinutesChallenge.getDurationString(70, -1));
+        System.out.println(SecondsAndMinutesChallenge.getDurationString(-1, 59));
 
         System.out.println();
 
-        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
-        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
-        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
-
-        System.out.println();
-
-        System.out.println(BarkingDog.shouldWakeUp(true, 1));
-        System.out.println(BarkingDog.shouldWakeUp(false, 2));
-        System.out.println(BarkingDog.shouldWakeUp(true, 8));
-        System.out.println(BarkingDog.shouldWakeUp(true, -1));
-
-        System.out.println();
-
-        System.out.println(LeapYear.isLeapYear(-1600));
-        System.out.println(LeapYear.isLeapYear(1600));
-        System.out.println(LeapYear.isLeapYear(2017));
-        System.out.println(LeapYear.isLeapYear(2000));
-        System.out.println(LeapYear.isLeapYear(1924));
-
-        System.out.println();
-
-        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.1756, -3.175));
-        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.175, 3.176));
-        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.0, 3.0));
-        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.123, 3.123));
-
-        System.out.println();
-
-        System.out.println(EqualSumChecker.hasEqualSum(1,1,1));
-        System.out.println(EqualSumChecker.hasEqualSum(1,1,2));
-        System.out.println(EqualSumChecker.hasEqualSum(1,-1,0));
-
-        System.out.println();
-
-        System.out.println(TeenNumberChecker.hasTeen(9,99,19));
-        System.out.println(TeenNumberChecker.hasTeen(23,15,42));
-        System.out.println(TeenNumberChecker.hasTeen(22,23,34));
+        System.out.println(SecondsAndMinutesChallenge.getDuration(36000));
+        System.out.println(SecondsAndMinutesChallenge.getDuration(0));
+        System.out.println(SecondsAndMinutesChallenge.getDuration(-1));
     }
 
 }
