0091e692a4e120a983a72b02c10237252b508d25
Mariah Baysic <mjpb123@gmail.com>
2019-02-10 02:01:20 +0800

    Runner.java - commented out MinutesToYearsDaysCalculator, added IntEqualityPrinter; IntEqualityPrinter.java - determines if 3 numbers are all equal, different, or neither

--- src/Runner.java
+++ src/Runner.java
@@ -69,12 +69,19 @@ public class Runner {
 //        System.out.println(AreaCalculator.area(-1));
 //        System.out.println(AreaCalculator.area(5.0,4.0));
 //        System.out.println(AreaCalculator.area(-1.0, 4.0));
+//
+//        System.out.println();
+//
+//        MinutesToYearsDaysCalculator.printYearsAndDays(525600);
+//        MinutesToYearsDaysCalculator.printYearsAndDays(1051200);
+//        MinutesToYearsDaysCalculator.printYearsAndDays(561600);
 
         System.out.println();
 
-        MinutesToYearsDaysCalculator.printYearsAndDays(525600);
-        MinutesToYearsDaysCalculator.printYearsAndDays(1051200);
-        MinutesToYearsDaysCalculator.printYearsAndDays(561600);
+        IntEqualityPrinter.printEqual(1,1,1);
+        IntEqualityPrinter.printEqual(1,1,2);
+        IntEqualityPrinter.printEqual(-1,-1,-1);
+        IntEqualityPrinter.printEqual(1,2,3);
     }
 
 }
